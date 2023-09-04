a7177cc63779692c99a75bc13a1681290264e359
Nikita Nikita <jassie.jassie@yandex.ru>
2023-09-04 20:48:54 +0300

    Параметр in (в данном случае не работает)

--- Module5App/Program.cs
+++ Module5App/Program.cs
@@ -3,25 +3,19 @@ using System.Reflection.Metadata;
 
 class MainClass
 {
-    static void ChangeAge(ref int age)
-    {
-        Console.WriteLine("Enter age");
-        age = int.Parse(Console.ReadLine());
-    }
-    static void GetName(ref string name)
-    {
-        Console.WriteLine("Enter name");
-        name = Console.ReadLine();
-    }
     public static void Main(string[] args)
     {
-        var someName = "Евгений";
-        Console.WriteLine(someName);
-        GetName(ref someName);      
-        Console.WriteLine(someName);
-        var someAge = 18;
-        ChangeAge(ref someAge);
-        Console.WriteLine(someAge);
+        var arr = new int[] { 1, 2, 3 };
+        BigDataOperation(arr);
+        Console.WriteLine(arr[0]);
+        Console.WriteLine(arr[1]);
+        Console.WriteLine(arr[2]);
     }
-}
+    static void BigDataOperation(in int[] arr)
+    {
+        arr[0] = 4;
+        arr[1] = 44;
+        arr[2] = 444;
+    }
 
+}
