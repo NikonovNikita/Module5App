3319dc6d91cd7f8f82bf0c615f80aaa58babc286
Nikita Nikita <jassie.jassie@yandex.ru>
2023-09-04 20:34:45 +0300

    Задание 5.3.3 Передача параметров в метод по ссылке

--- Module5App/Program.cs
+++ Module5App/Program.cs
@@ -3,63 +3,25 @@ using System.Reflection.Metadata;
 
 class MainClass
 {
+    static void ChangeAge(ref int age)
+    {
+        Console.WriteLine("Enter age");
+        age = int.Parse(Console.ReadLine());
+    }
+    static void GetName(ref string name)
+    {
+        Console.WriteLine("Enter name");
+        name = Console.ReadLine();
+    }
     public static void Main(string[] args)
     {
-        (string name, int age) Anketa;
-        Console.Write("Enter your name ");
-        Anketa.name = Console.ReadLine();
-        Console.Write("Enter your age in numbers ");
-        Anketa.age = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Your name is {0}", Anketa.name);
-        Console.WriteLine("Your age is {0}", Anketa.age);
-
-        string[] favColors = new string[3];
-        for(int i = 0; i < favColors.Length; i++)
-        {
-        favColors[i] = ShowColor(Anketa.name);
-        }
-
-        Console.WriteLine("Ваши любимые цвета:");
-        foreach (var num in favColors)
-        {
-            Console.Write(num + " ");
-        }
-    }
-    
-    static string ShowColor(string UserName = "Jane", params string[] favColors)
-    {
-        Console.Write("{0},\nнапишите свой любимый цвет на английском с маленькой буквы ",UserName);
-        var Color = Console.ReadLine();
-        switch (Color)
-        {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is red!");
-                break;
-
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-                break;
-
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-                break;
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is yellow!");
-                break;
-        }
-        return Color;
+        var someName = "Евгений";
+        Console.WriteLine(someName);
+        GetName(ref someName);      
+        Console.WriteLine(someName);
+        var someAge = 18;
+        ChangeAge(ref someAge);
+        Console.WriteLine(someAge);
     }
 }
+
