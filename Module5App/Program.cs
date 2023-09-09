3212d7f7a1a7bff5b71f5970f280c9353b5528b3
Nikita Nikita <jassie.jassie@yandex.ru>
2023-09-09 20:34:03 +0300

    Пустое

--- Module5App/Program.cs
+++ Module5App/Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 using System.Reflection.Metadata;
 
-class MainClass
+class Program
 {
     public static void Main(string[] args)
     {
-        var arr = new int[] { 1, 2, 3 };
-        BigDataOperation(arr);
-        Console.WriteLine(arr[0]);
-        Console.WriteLine(arr[1]);
-        Console.WriteLine(arr[2]);
+        
     }
-    static void BigDataOperation(in int[] arr)
+    static int ()
     {
-        arr[0] = 4;
-        arr[1] = 44;
-        arr[2] = 444;
+        
     }
+    
 
 }
